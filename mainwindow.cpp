ab27ede5db588e5017b10fb241ee00d27f361d87
usershaman <usershaman@gmail.com>
2013-09-21 13:14:00 -0400

    qdebug test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug() << "hello";
 }
 
 MainWindow::~MainWindow()
